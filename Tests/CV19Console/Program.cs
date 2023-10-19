c1e038c9849233b98515af358a955e1ce45c7a3c
Alex Mironositskiy <drive_forever@mail.ru>
2023-10-19 11:32:44 +0200

    Initial commit

---
+++ Tests/CV19Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CV19Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
