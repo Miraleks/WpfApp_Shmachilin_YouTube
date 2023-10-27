7416012735dd1c8c3a04d32284076c3a65aa6932
Alex Mironositskiy <drive_forever@mail.ru>
2023-10-27 09:48:04 +0200

    change CV19Console project from .NetFramework 4.6 to .Net

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19Console
 {
     internal class Program
     {
         const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        private static async Task<Stream> GetDataStream()
+        {
+            var client = new HttpClient();
+            var responce = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await responce.Content.ReadAsStreamAsync();
+        }
+
+        private static async IEnumerable<string> GetDataLines()
+        {
+            await using var data_stream = await GetDataStream();
+            using var data_reader = new StreamReader(data_stream);
+
+            while (!data_reader.EndOfStream)
+            {
+                var line = await data_reader.ReadLineAsync();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+            }
+        }
 
         static void Main(string[] args)
         {
